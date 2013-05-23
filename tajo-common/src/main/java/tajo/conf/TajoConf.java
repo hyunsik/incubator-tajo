cd6c467ce1c736b0613bb95ec24f52f817d9c4d1
Hyunsik Choi <hyunsik@apache.org>
2013-05-24 03:16:30 +0900

    Fixed some parameters.

--- tajo-common/src/main/java/tajo/conf/TajoConf.java
+++ tajo-common/src/main/java/tajo/conf/TajoConf.java
@@ -108,9 +108,9 @@ public class TajoConf extends YarnConfiguration {
     // Distributed Query Execution Parameters
     //////////////////////////////////////////
     SORT_BUFFER_SIZE("tajo.sort.mb", 512),
-    JOIN_TASK_VOLUME("tajo.join.task-volume.mb", 512),
-    SORT_TASK_VOLUME("tajo.sort.task-volume.mb", 512),
-    AGGREGATION_TASK_VOLUME("tajo.task-aggregation.volume.mb", 512),
+    JOIN_TASK_VOLUME("tajo.join.task-volume.mb", 128),
+    SORT_TASK_VOLUME("tajo.sort.task-volume.mb", 128),
+    AGGREGATION_TASK_VOLUME("tajo.task-aggregation.volume.mb", 128),
 
     JOIN_PARTITION_VOLUME("tajo.join.part-volume.mb", 512),
     SORT_PARTITION_VOLUME("tajo.sort.part-volume.mb", 512),
