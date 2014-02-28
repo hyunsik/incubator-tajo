9b397afb04c53d649c849a9c92e1af5394856d08
Hyunsik Choi <hyunsik@apache.org>
2014-02-28 16:35:13 +0900

    changed css.

--- tajo-docs/src/main/sphinx/conf.py
+++ tajo-docs/src/main/sphinx/conf.py
@@ -117,7 +117,11 @@ html_theme = 'haiku'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+ "linkcolor" : "#577492",
+ "visitedlinkcolor" : "#577492",
+ "hoverlinkcolor" : "#551A8B"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
